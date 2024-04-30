bad15a3dc7eebc002bbbda83706c983b87614f77
XolaniMthembu <107764996+XolaniMthembu@users.noreply.github.com>
2024-04-30 13:27:09 +0200

    Add files via upload
    Counting using for loop statement iteration

---
+++ ForLoop.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ForLoop{
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        
+        for(int i = 10; i >= 1; i--){
+            
+            System.out.println(i);
+        }
+        
+            System.out.println("Happy New Year!!!");
+    
+    }
+}
